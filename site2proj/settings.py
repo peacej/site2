7cccd736e542a8ed9d7f154a80caf603d023e8ed
Jerry Chi <jchi@indeed.com>
2022-07-22 21:57:30 +0900

    add CSRF

--- site2proj/settings.py
+++ site2proj/settings.py
@@ -123,3 +123,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import django_heroku
 django_heroku.settings(locals())
+CSRF_TRUSTED_ORIGINS = ["https://jer-site2.herokuapp.com"]
